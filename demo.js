60b1d104e1ea0028fad40c21feb499f1b2defa4f
ronen-halevy <ronen567@gmail.com>
2023-01-15 21:17:12 +0200

    await

--- demo.js
+++ demo.js
@@ -4,55 +4,57 @@ import Render from './Render.js';
 import configModel from './configs/configModel.json' assert { type: 'json' };
 import configNms from './configs/configNms.json' assert { type: 'json' };
 
-const modelsTable = configModel.models;
-// take first in list as a default:
-const selectedModel = Object.keys(modelsTable)[0];
-console.log(selectedModel);
+const demoYovoV3 = async () => {
+  const modelsTable = configModel.models;
+  // take first in list as a default:
+  const selectedModel = Object.keys(modelsTable)[0];
+  console.log(selectedModel);
 
-const selectedWeights = Object.keys(modelsTable[selectedModel])[0];
-console.log(selectedWeights);
-console.log(configModel);
+  const selectedWeights = Object.keys(modelsTable[selectedModel])[0];
+  console.log(selectedWeights);
+  console.log(configModel);
 
-const modelParams = configModel['models'][selectedModel][selectedWeights];
+  const modelParams = configModel['models'][selectedModel][selectedWeights];
 
-const { modelUrl, anchorsUrl, classNamesUrl } = modelParams;
-console.log(modelUrl, anchorsUrl, classNamesUrl);
-// const
-let [model, anchors, classNames] = await createModel(
-  modelUrl,
-  anchorsUrl,
-  classNamesUrl
-);
+  const { modelUrl, anchorsUrl, classNamesUrl } = modelParams;
+  console.log(modelUrl, anchorsUrl, classNamesUrl);
+  // const
+  let [model, anchors, classNames] = await createModel(
+    modelUrl,
+    anchorsUrl,
+    classNamesUrl
+  );
 
-classNames = classNames.split(/\r?\n/);
-const nClasses = classNames.length;
-console.log([classNames]);
-console.log(nClasses);
+  classNames = classNames.split(/\r?\n/);
+  const nClasses = classNames.length;
+  console.log([classNames]);
+  console.log(nClasses);
 
-const { scoreTHR, iouTHR, maxBoxes } = configNms;
+  const { scoreTHR, iouTHR, maxBoxes } = configNms;
 
-const yoloV3 = new YoloV3(
-  model,
-  anchors.anchor,
-  nClasses,
-  scoreTHR,
-  iouTHR,
-  maxBoxes
-);
+  const yoloV3 = new YoloV3(
+    model,
+    anchors.anchor,
+    nClasses,
+    scoreTHR,
+    iouTHR,
+    maxBoxes
+  );
 
-const imgUrl1 =
-  'https://www.shutterstock.com/shutterstock/photos/1262270857/display_1500/stock-photo-curvy-windy-road-in-snow-covered-forest-top-down-aerial-view-1262270857.jpg';
+  const imgUrl =
+    'https://www.shutterstock.com/shutterstock/photos/1262270857/display_1500/stock-photo-curvy-windy-road-in-snow-covered-forest-top-down-aerial-view-1262270857.jpg';
 
-var imageObject = new window.Image();
+  var imageObject = new window.Image();
 
-const res = await fetch(imgUrl1);
-const imageBlob = await res.blob();
-const imageObjectURL = URL.createObjectURL(imageBlob);
-imageObject.src = imageObjectURL;
-imageObject.addEventListener('load', async () => {
-  const f = yoloV3.detectFrame(imageObject).then((res) => {
-    console.log(res);
+  const res = await fetch(imgUrl);
+  const imageBlob = await res.blob();
+  const imageObjectURL = URL.createObjectURL(imageBlob);
+  imageObject.src = imageObjectURL;
+  imageObject.addEventListener('load', async () => {
+    const f = await yoloV3.detectFrame(imageObject);
+    console.log(f);
   });
-});
+  console.log(selectedWeights);
+};
 
-console.log(selectedWeights);
+demoYovoV3();
