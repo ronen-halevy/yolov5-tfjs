fe3604aba858b6ce13ea1eba48c2302c1c1a258c
ronen-halevy <ronen567@gmail.com>
2023-01-21 01:02:48 +0200

    arraange+disable button before load

--- demo.js
+++ demo.js
@@ -5,27 +5,12 @@ import configModel from './configs/configModel.json' assert { type: 'json' };
 import configNms from './configs/configNms.json' assert { type: 'json' };
 import configRender from './configs/configRender.json' assert { type: 'json' };
 
-var yoloV3 = '';
-var classNames = '';
-
 const modelsTable = configModel.models;
+var classNames = '';
+var yoloV3 = '';
+
 const imageUrl =
 	'https://cdn.pixabay.com/photo/2023/01/01/16/35/street-7690347_960_720.jpg';
-
-const font = configRender.font;
-const lineWidth = configRender.lineWidth;
-const lineColor = configRender.lineColor;
-const textColor = configRender.textColor;
-const textBackgoundColor = configRender.textBackgoundColor;
-
-const draw = new Render(
-	canvas,
-	lineWidth,
-	lineColor,
-	font,
-	textColor,
-	textBackgoundColor
-);
 
 const onSelectModel = async (event) => {
 	$('#runYolo').attr('disabled', true);
@@ -58,7 +43,7 @@ const onSelectModel = async (event) => {
 	$('#runYolo').attr('disabled', false);
 };
 
-const onClickRunYolo = async (yoloV3, imageUrl) => {
+const onClickRunYolo = async (yoloV3, draw, imageUrl) => {
 	$('#runYoloSpinner').show();
 	var imageObject = new window.Image();
 	const res = await fetch(imageUrl);
@@ -83,8 +68,28 @@ const onClickRunYolo = async (yoloV3, imageUrl) => {
 };
 
 $(document).ready(function () {
+	// disable button before any model loaded
+	$('#runYolo').attr('disabled', true);
+	// disable spinners
 	$('#loadingModelSpinner').hide();
 	$('#runYoloSpinner').hide();
+
+	// init renderer:
+	const font = configRender.font;
+	const lineWidth = configRender.lineWidth;
+	const lineColor = configRender.lineColor;
+	const textColor = configRender.textColor;
+	const textBackgoundColor = configRender.textBackgoundColor;
+
+	const draw = new Render(
+		canvas,
+		lineWidth,
+		lineColor,
+		font,
+		textColor,
+		textBackgoundColor
+	);
+	// init model selection radio buttons:
 	const models = Object.keys(modelsTable);
 	models.map((option, index) => {
 		$('#divRadioSelectModel')
@@ -102,5 +107,6 @@ $(document).ready(function () {
 			)
 			.append($('<br>'));
 	});
-	$('#runYolo').click(onClickRunYolo(yoloV3, imageUrl));
+	// assign run button cbk
+	$('#runYolo').click(() => onClickRunYolo(yoloV3, draw, imageUrl));
 });
