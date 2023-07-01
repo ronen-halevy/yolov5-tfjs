4e49f487fa737fdff1b9be47d02aa33d384df5a0
ronen-halevy <ronen567@gmail.com>
2023-07-01 17:52:00 +0300

    render module for cdn

--- utils/createModule.js
+++ utils/createModule.js
@@ -22,45 +22,57 @@ node createModule.js
 * @license LicenseHereIfApplicable
 
 */
-const src = 'YoloV5.js';
-const dst = 'YoloV5.module.js';
-var fs = require('fs');
 
-// line to remove:
-removeLine = 'export { YoloV5, createModel };';
-newLine =
-	'const yolov5 = {YoloV5: YoloV5, createModel: createModel} \nmodule.exports = yolov5';
+const createModuleFile = (src, dst, removeLine, newLine, fs) => {
+	// 1. Remove line
+	fs.readFile(src, { encoding: 'utf-8' }, function (err, data) {
+		if (err) throw error;
 
-// 1. Remove line
-fs.readFile(src, { encoding: 'utf-8' }, function (err, data) {
-	if (err) throw error;
+		let dataArray = data.split('\n'); // convert file data in an array
+		let lastIndex = -1;
 
-	let dataArray = data.split('\n'); // convert file data in an array
-	let lastIndex = -1;
-
-	for (let index = 0; index < dataArray.length; index++) {
-		if (dataArray[index].includes(removeLine)) {
-			// check if a line contains the 'user1' keyword
-			lastIndex = index; // found a line includes a 'user1' keyword
-			break;
+		for (let index = 0; index < dataArray.length; index++) {
+			if (dataArray[index].includes(removeLine)) {
+				// check if a line contains the 'user1' keyword
+				lastIndex = index; // found a line includes a 'user1' keyword
+				break;
+			}
 		}
-	}
 
-	// write to new file
-	dataArray.splice(lastIndex, 1);
+		// write to new file
+		dataArray.splice(lastIndex, 1);
 
-	const updatedData = dataArray.join('\n');
-	console.log('write!!!!');
-	fs.writeFile(dst, updatedData, (err) => {
-		if (err) throw err;
-		console.log('Successfully updated the file data');
-		// append new line:
-		fs.open(dst, 'a', 666, function (e, id) {
-			fs.write(id, newLine + '\r\n', null, 'utf8', function () {
-				fs.close(id, function () {
-					console.log('file is updated');
+		const updatedData = dataArray.join('\n');
+		console.log('write!!!!');
+		fs.writeFile(dst, updatedData, (err) => {
+			if (err) throw err;
+			console.log('Successfully updated the file data');
+			// append new line:
+			fs.open(dst, 'a', 666, function (e, id) {
+				fs.write(id, newLine + '\r\n', null, 'utf8', function () {
+					fs.close(id, function () {
+						console.log('file is updated');
+					});
 				});
 			});
 		});
 	});
-});
+};
+
+var src = 'YoloV5.js';
+var dst = 'YoloV5.module.js';
+var fs = require('fs');
+
+// line to remove:
+var removeLine = 'export { YoloV5, createModel };';
+var newLine =
+	'const yolov5 = {YoloV5: YoloV5, createModel: createModel} \nmodule.exports = yolov5';
+createModuleFile(src, dst, removeLine, newLine, fs);
+
+var src = 'Render.js';
+var dst = 'Render.module.js';
+
+// line to remove:
+var removeLine = 'export { Render };';
+var newLine = 'const render = {Render: Render} \nmodule.exports = render';
+createModuleFile(src, dst, removeLine, newLine, fs);
